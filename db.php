95f876173bd32170afb506365766ced16c48e4f9
Lauriacunia <lauriacunia@gmail.com>
2021-06-29 17:22:21 -0300

    Termino CRUD de tareas.

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    'tu_contraseña',
+    'php_mysql_crud'
+);
+
+/*
+if (isset($conn)) {
+    echo 'DB is connected';
+
+} */
+?>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP mySQL CRUD</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD
+        </a>
+    </div>
+</nav>
